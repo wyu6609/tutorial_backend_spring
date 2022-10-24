29a840f8b75dbe9fe57629b3d9dff99b9e5e938e
William Yu <88666309+wyu6609@users.noreply.github.com>
2022-10-24 19:10:04 -0400

    Tutorial Repository built

--- src/main/java/com/example/tutorialbackend/repository/TutorialRepository.java
+++ src/main/java/com/example/tutorialbackend/repository/TutorialRepository.java
@@ -1,4 +1,23 @@
 package com.example.tutorialbackend.repository;
 
-public class TutorialRepository {
+import com.example.tutorialbackend.model.Tutorial;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
+
+    List<Tutorial> findByPublished(boolean published);
+    List<Tutorial> findByTitleContaining(String title);
+
+
 }
+
+
+//    Now we can use JpaRepository’s methods: save(), findOne(), findById(), findAll(), count(), delete(), deleteById()… without implementing these methods.
+//
+//        We also define custom finder methods:
+//        – findByPublished(): returns all Tutorials with published having value as input published.
+//        – findByTitleContaining(): returns all Tutorials which title contains input title.
+//
+//        The implementation is plugged in by Spring Data JPA automatically.
